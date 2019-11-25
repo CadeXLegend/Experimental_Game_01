0f7cef55bb43fe0eb5b3b36cfd56ff2fff7e281c
CadeXDev <37698676+CadeXLegend@users.noreply.github.com>
2019-11-25 21:58:03 +1000

    - Tile Decoration which gets placed as an overlay over the base Tiles

---
+++ Experimental_Game_01/Assets/Scripts/Grid Generation/ScriptableObjects/TileDecoration.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// ScriptableObject containing Sprites to be applied to a Grid.
+/// </summary>
+[CreateAssetMenu(fileName = "New Tile Decoration", menuName ="Grid/Tile Decoration", order = 0)]
+public class TileDecoration : ScriptableObject
+{
+    public Sprite Decoration;
+    private float Height;
+}

57bcb51cf7d9de915a4f32acd884ab751073d378
CadeXDev <37698676+CadeXLegend@users.noreply.github.com>
2019-11-27 02:18:17 +1000

    - Agent config for doing things with Agents such as moving them, telling them tto do things, etc, idk

---
+++ Experimental_Game_01/Assets/Scripts/Agent Scripts/MoverFromInput.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Agent
+{
+    /// <summary>
+    /// Moves the Agent from Input Received.
+    /// </summary>
+    //we only want to do movement calculations
+    //with the physics engine, not translate,
+    //or any vector math outside of the fixedupdate physics cycle
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class MoverFromInput : MonoBehaviour, IMover
+    {
+        private Rigidbody2D rb;
+
+        /// <summary>
+        /// The configuration file containing Agent information to be applied to this class's parameters.
+        /// </summary>
+        [SerializeField]
+        private AgentConfig agentConfiguration;
+        [SerializeField]
+        private float movementSpeed;
+        // Start is called before the first frame update
+        public void Init(AgentConfig config)
+        {
+            agentConfiguration = config;
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        private void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
+            movementSpeed = agentConfiguration.MovementSpeed;
+        }
+
+        // Update is called once per frame
+        private void FixedUpdate()
+        {
+            float x = Input.GetAxis("Horizontal");
+            float y = Input.GetAxis("Vertical");
+            Vector2 movementDirection = new Vector2(x, y);
+
+            Move(movementDirection);
+        }
+
+        public virtual void Move(Vector2 direction)
+        {
+            if (!agentConfiguration.UsesRigidbody2D)
+                return;
+
+            rb.MovePosition((Vector2)transform.position + (direction * movementSpeed * Time.fixedDeltaTime));
+        }
+    }
+}
---
+++ Experimental_Game_01/Assets/Scripts/Agent Scripts/ScriptableObjects/AgentConfig.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Agent
+{
+
+    /// <summary>
+    /// ScriptableObject containing Agent information.
+    /// </summary>
+    [CreateAssetMenu(fileName = "New Agent Configuration", menuName = "Agents/New Agent Config", order = 0)]
+    public class AgentConfig : ScriptableObject
+    {
+        /// <summary>
+        /// The Agent's Graphic Sprite to be Rendered.
+        /// </summary>
+        [Header("Agent Parameters")]
+        [Tooltip("The Agent's Graphic Sprite to be Rendered.")]
+        public Sprite Graphic;
+        /// <summary>
+        /// Whether or not the Agent will use a Rigidbody 2D for Physics based controls.
+        /// </summary>
+        [Tooltip("Whether or not the Agent will use a Rigidbody 2D for Physics based controls.")]
+        public bool UsesRigidbody2D;
+        /// <summary>
+        /// The rate at which the Agent will move.
+        /// </summary>
+        [Tooltip("The rate at which the Agent will move.")]
+        public float MovementSpeed;
+    }
+}

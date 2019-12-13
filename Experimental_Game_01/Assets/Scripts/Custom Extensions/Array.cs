194fcb1e232e1cafdb2e86ec24c45fe25dda4e0f
CadeXDev <37698676+CadeXLegend@users.noreply.github.com>
2019-12-13 20:13:23 +1000

    I made this to do Array.ForEach on a Multidimensional array (I made it specifically for 2D Arrays, I could've made it N arrays but it doesn't serve my needs.)
    It's pretty neat and self explanatory so yay.

---
+++ Experimental_Game_01/Assets/Scripts/Custom Extensions/Array.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+/// <summary>
+/// Custom class for Multidimensional Array Traversals.
+/// </summary>
+public static class Array
+{
+    /// <summary>
+    /// Traverse a Multidimensional Array and run code inside giving acccess to Column & Row definition.
+    /// </summary>
+    /// <typeparam name="T">The Type of Array.</typeparam>
+    /// <param name="MultidimensionalArray">The Array itself.</param>
+    /// <param name="action">The action to run inside of the Traversal.</param>
+    public static void ForEach<T>(T[,] MultidimensionalArray, Action<int, int> action)
+    {
+        int columns = MultidimensionalArray.GetLength(0);
+        int rows = MultidimensionalArray.GetLength(1);
+        for (int column = 0; column < columns; ++column)
+        {
+            for (int row = 0; row < rows; ++row)
+            {
+                action(column, row);
+            }
+        }
+    }
+}

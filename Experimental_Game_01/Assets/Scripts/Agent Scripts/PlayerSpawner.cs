fb88d89569143c28348ab53fd824d0465cf345d3
CadeXDev <37698676+CadeXLegend@users.noreply.github.com>
2019-12-13 20:18:48 +1000

    Moved the Spawn to the PlayerSpawner and dedicated it to handling spawning Players into the game/map.

--- Experimental_Game_01/Assets/Scripts/Agent Scripts/PlayerSpawner.cs
+++ Experimental_Game_01/Assets/Scripts/Agent Scripts/PlayerSpawner.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using MyBox;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -19,16 +20,14 @@ namespace Agent
         [SerializeField]
         private Transform SpawnedAgentsParent;
 
-        private SpawnAgent agentSpawner;
-
         private List<GameObject> SpawnedPlayers = new List<GameObject>();
         // Start is called before the first frame update
         void Start()
         {
-            agentSpawner = new SpawnAgent();
             generator.MapGenerated += SpawnPlayers;
         }
 
+        [ButtonMethod]
         public virtual void SpawnPlayers()
         {
             if (SpawnedPlayers.Count > 0)
@@ -38,11 +37,28 @@ namespace Agent
             }
 
             for (int i = 0; i < AmountOfPlayers; ++i)
-                SpawnedPlayers.Add(agentSpawner.Spawn(
+                SpawnedPlayers.Add(Spawn(
                     playerPrefabs[i],      //the player's prefab/gameobject to spawn
                     playerDatas[i],        //the player's data to bind to the player's components
                     generator.Grid.GetRandomUnoccupiedTile(Generation.Tile.PositionOnGrid.Center).transform.position, //the player's tile to spawn on
                     SpawnedAgentsParent));  //the parent the player will be a child of
+        }
+
+        public virtual GameObject Spawn(GameObject prefab, AgentConfig agentData, Vector3 position, Transform parent)
+        {
+            #region Instantiation & Component Initialization
+            prefab = GameObject.Instantiate(prefab, position, Quaternion.identity, parent);
+            Agent agent = prefab.GetComponent<Agent>();
+            MoverFromInput mover = prefab.AddComponent<MoverFromInput>();
+            UnityEditor.CAutoInjectionEditor.InjectFor_CurrentScene();
+            #endregion
+
+            #region Data Binding
+            mover.Init(agentData);
+            agent.Init(agentData, mover);
+            #endregion
+
+            return prefab;
         }
     }
 }

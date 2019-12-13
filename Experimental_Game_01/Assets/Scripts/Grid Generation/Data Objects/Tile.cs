e285d5b0981898f0289ce93729ffd7754242059a
CadeXDev <37698676+CadeXLegend@users.noreply.github.com>
2019-12-13 20:27:00 +1000

    Added Neighbours and a quick implementation for making a Tile's Neighbours visible.
    Mouse Down on Tile (Node n in Graph g), show Neighbours by tinting colour and show original Tile in unique colour (all/or)[left,right,up,down].
    
    Mouse Up, reset them to their previous colours.

--- Experimental_Game_01/Assets/Scripts/Grid Generation/Data Objects/Tile.cs
+++ Experimental_Game_01/Assets/Scripts/Grid Generation/Data Objects/Tile.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace Generation
@@ -22,6 +23,8 @@ namespace Generation
         }
 
         //the properties with [SerializeField] [ReadOnly] are for debugging & information purposes in the inspector
+        private List<Tile> neighbours;
+        public List<Tile> Neighbours { get => neighbours; }
         [Header("Tile Information")]
         [SerializeField] [ReadOnly] private PositionOnGrid tilePositionOnGrid;
         public PositionOnGrid TilePositionOnGrid { get => tilePositionOnGrid; set => tilePositionOnGrid = value; }
@@ -39,6 +42,29 @@ namespace Generation
         {
             TilePositionOnGrid = _TilePositionOnGrid;
             CoordinatesOnGrid = _CoordinatesOnGrid;
+        }
+
+        public void AssignNeighbours(List<Tile> _neighbours)
+        {
+            neighbours = _neighbours;
+        }
+
+        Dictionary<Tile, Color32> lastSelectedTiles = new Dictionary<Tile, Color32>();
+        private void OnMouseDown()
+        {
+            foreach (Tile n in neighbours)
+            {
+                lastSelectedTiles.Add(n, n.spriteRenderer.color);
+                n.spriteRenderer.color = new Color32(255, 0, 255, 100);
+            }
+            lastSelectedTiles.Add(this, spriteRenderer.color);
+            spriteRenderer.color = new Color32(255, 0, 0, 100);
+        }
+        private void OnMouseUp()
+        {
+            foreach (KeyValuePair<Tile, Color32> pair in lastSelectedTiles)
+                pair.Key.spriteRenderer.color = pair.Value;
+            lastSelectedTiles.Clear();
         }
     }
 }

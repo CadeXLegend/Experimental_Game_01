cb53a11b2ec4675b3706cccff14b0cc6ef4b4466
CadeXDev <37698676+CadeXLegend@users.noreply.github.com>
2019-12-13 20:16:32 +1000

    All the Unity asset stuff, prefab changes, scene modifications, etc...

--- Experimental_Game_01/Assets/Tests/Grid Generation Tests/TileTests.cs
+++ Experimental_Game_01/Assets/Tests/Grid Generation Tests/TileTests.cs
@@ -16,8 +16,8 @@ namespace Tests
             // Use the Assert class to test conditions
             Tile t1 = new GameObject().AddComponent<Tile>();
             Tile t2 = new GameObject().AddComponent<Tile>();
-            t1.Init(Tile.PositionOnGrid.BottomEdgeColumn);
-            t2.Init(Tile.PositionOnGrid.BottomEdgeColumn, new Vector2(0, 1));
+            //t1.Init(Tile.PositionOnGrid.BottomEdgeColumn);
+            //t2.Init(Tile.PositionOnGrid.BottomEdgeColumn, new Vector2(0, 1));
 
             Assert.That(t1.TilePositionOnGrid == Tile.PositionOnGrid.BottomEdgeColumn);
             Assert.That(t2.CoordinatesOnGrid == new Vector2(0, 1));

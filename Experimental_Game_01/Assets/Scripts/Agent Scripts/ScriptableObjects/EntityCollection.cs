f437099758832450bb3cb75700f0fd9e035df5da
CadeXDev <37698676+CadeXLegend@users.noreply.github.com>
2019-12-13 20:13:44 +1000

    Legit ignore this for now.

---
+++ Experimental_Game_01/Assets/Scripts/Agent Scripts/ScriptableObjects/EntityCollection.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Agent
+{
+    /// <summary>
+    /// ScriptableObject containing Entities.
+    /// </summary>
+    [CreateAssetMenu(fileName = "New Entity Collection", menuName = "Entities/New Entity Collection", order = 0)]
+    public class EntityCollection : ScriptableObject
+    {
+        [Header("Put your Entities here.")]
+        public GameObject[] Entities;
+    }
+}

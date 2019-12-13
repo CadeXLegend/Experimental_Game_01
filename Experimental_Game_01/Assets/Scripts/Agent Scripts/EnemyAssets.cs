177aa8f51a6358f886f81b08632d0a7164120432
CadeXDev <37698676+CadeXLegend@users.noreply.github.com>
2019-12-13 20:15:26 +1000

    Data container for enemy assets

---
+++ Experimental_Game_01/Assets/Scripts/Agent Scripts/EnemyAssets.cs
@@ -0,0 +1,19 @@
+﻿using Agent;
+using Generation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Let's pretend this class doesn't exist until I solidify my approach to doing this stuff.
+/// I am writing this while being very tired...
+/// </summary>
+public class EnemyAssets : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject enemySpawner;
+    public GameObject EnemySpawner { get => enemySpawner; }
+    [SerializeField]
+    private EntityCollection enemies;
+    public EntityCollection Enemies { get => enemies; }
+}

dda7cfa775af1d3022da16edbb63ee9ab6243a5f
CadeXDev <37698676+CadeXLegend@users.noreply.github.com>
2019-12-24 08:29:25 +1000

    Added: - Spawning Agents over time from a Spawner - Spawning in the default enemy Spawners
    Removed:
    - Removed EnemySpawner.cs cause don't need it
           - Split the old script into two new and improved scripts
           - Refactored a lot of it's code into better, cleaner code in the new split scripts

---
+++ Experimental_Game_01/Assets/Scripts/Agent Scripts/SpawnAgentOverTime.cs
@@ -0,0 +1,72 @@
+﻿using Generation;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAgentOverTime : MonoBehaviour
+{
+    private GridAssetSpawner assetSpawner;
+
+    [Header("Spawn Information")]
+    [SerializeField]
+    private float initialSpawnTime;
+    [SerializeField]
+    private float spawnTimeIncrement;
+    private float actualSpawnTime;
+    private float ActualSpawnTime
+    {
+        get
+        {
+            return actualSpawnTime /= spawnTimeIncrement;
+        }
+    }
+
+    [SerializeField]
+    private GameObject agentToSpawn;
+    [SerializeField]
+    private Agent.AgentConfig agentConfig;
+    private Tile currentTile;
+
+    /// <summary>
+    /// Initialize this Object with the defined parameters.
+    /// </summary>
+    /// <param name="_assetSpawner">Asset Spawner required to call spawning functions</param>
+    /// <param name="_currentTile">The tile this object is currently situated on</param>
+    public void Init(GridAssetSpawner _assetSpawner, Tile _currentTile)
+    {
+        currentTile = _currentTile;
+        assetSpawner = _assetSpawner;
+    }
+
+    private void Awake()
+    {
+        actualSpawnTime = initialSpawnTime;
+        SpawnEnemyOnTick();
+    }
+
+    internal virtual void SpawnEnemyOnTick()
+    {
+        InvokeRepeating("SpawnEnemy", initialSpawnTime, ActualSpawnTime);
+    }
+
+    internal virtual void SpawnEnemy()
+    {
+        if (currentTile == null) return;
+        if (assetSpawner == null) return;
+
+        try
+        {
+            GameObject go = assetSpawner.SpawnAssetGeneric(
+                agentToSpawn,  //the agent we want to spawn into the world
+                agentToSpawn.name, //the name of the agent, e.g: skeleton
+                currentTile, //the current tile the thing this is attached to is on
+                spawnOntoRandomTileNeighbour: true //whether we want this object to spawn automagically onto a randomly selected neighbour tile
+                );
+
+            AISuperSimpleMove aiMover = go.AddComponent<AISuperSimpleMove>();
+            aiMover.Init(agentConfig, go.transform.parent.GetComponent<Tile>());
+            go.GetComponent<Agent.Agent>().Init(agentConfig, aiMover);
+        }
+        catch { /*please don't do what I'm doing here*/ }
+    }
+}
---
+++ Experimental_Game_01/Assets/Scripts/Agent Scripts/SpawnDefaultEnemySpawners.cs
@@ -0,0 +1,46 @@
+﻿using Generation;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDefaultEnemySpawners : MonoBehaviour
+{
+    [Header("Dependencies")]
+    [SerializeField]
+    private MapGenerator mapGenerator;
+    [SerializeField]
+    private GridAssetSpawner assetSpawner;
+
+    [SerializeField]
+    private EnemyAssets assets;
+
+    private void Start()
+    {
+        mapGenerator.OnMapGenerated += SpawnEnemyGameObjects;
+    }
+
+    private void SpawnEnemyGameObjects()
+    {
+        List<GameObject> spawners = new List<GameObject>();
+        Tile.PositionOnGrid flags = 
+            (
+            Tile.PositionOnGrid.TopLeftCorner    | 
+            Tile.PositionOnGrid.TopRightCorner   | 
+            Tile.PositionOnGrid.BottomLeftCorner | 
+            Tile.PositionOnGrid.BottomRightCorner
+            );
+        IEnumerable<Enum> flagValues = flags.GetIndividualFlags();
+        foreach(Enum value in flagValues)
+            spawners.Add(
+                assetSpawner.SpawnAssetGeneric(
+                mapGenerator.Grid,
+                assets.EnemySpawner,
+                assets.EnemySpawner.name,
+                (Tile.PositionOnGrid)value
+                ));
+
+        foreach (GameObject go in spawners)
+            go.GetComponent<SpawnAgentOverTime>().Init(assetSpawner, go.transform.parent.GetComponent<Tile>());       
+    }
+}

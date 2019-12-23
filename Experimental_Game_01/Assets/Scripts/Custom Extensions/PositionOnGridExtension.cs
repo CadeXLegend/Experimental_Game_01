53e60f0446623774287124908a8401cd6472c783
CadeXDev <37698676+CadeXLegend@users.noreply.github.com>
2019-12-24 08:31:12 +1000

    Added: - Really nice extensions       - Some found, some made by me       - Some also altered to suit my needs/conventions, etc

---
+++ Experimental_Game_01/Assets/Scripts/Custom Extensions/PositionOnGridExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static Generation.Tile;
+
+public static class PositionOnGridExtension
+{
+    public static PositionOnGrid Previous(this PositionOnGrid position)
+    {
+        PositionOnGrid[] posArray = (PositionOnGrid[])Enum.GetValues(typeof(PositionOnGrid));
+        int previousIndex = System.Array.IndexOf(posArray, position) - 1;
+        return (previousIndex <= 0) ? posArray[0] : posArray[previousIndex];
+    }
+
+    public static PositionOnGrid Next(this PositionOnGrid position)
+    {
+        PositionOnGrid[] posArray = (PositionOnGrid[])Enum.GetValues(typeof(PositionOnGrid));
+        int nextIndex = System.Array.IndexOf(posArray, position) + 1;
+        return (nextIndex >= posArray.Length) ? posArray[0] : posArray[nextIndex];
+    }
+
+    public static int GetUnshiftedNumber(this PositionOnGrid position)
+    {
+        PositionOnGrid[] posArray = (PositionOnGrid[])Enum.GetValues(typeof(PositionOnGrid));
+        int posInIndex = System.Array.IndexOf(posArray, position);
+        return posInIndex;
+    }
+}

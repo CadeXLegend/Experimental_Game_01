ecf5836b87fd0b4245537221d33f4566f162605c
CadeXDev <37698676+CadeXLegend@users.noreply.github.com>
2019-12-24 08:30:33 +1000

    Added: - Setting up some AI movement systems         - Doing super super basic stuff rn to sort out basic logistics of things for AI movement
    Future Scope:
    - Going to be adding in Pathfinding, radius searching, and so on

---
+++ Experimental_Game_01/Assets/Scripts/Agent Scripts/AISuperSimpleMove.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Agent;
+using Generation;
+
+public class AISuperSimpleMove : MonoBehaviour, IMover
+{
+    private AgentConfig config;
+    private Tile lastTileWasOn;
+    private Tile currentTile;
+    private Tile tileToMoveTo;
+    //temp variable vvv
+    private float moveStep = 1f;
+
+    public void Init(AgentConfig _config, Tile _currentTile)
+    {
+        config = _config;
+        currentTile = _currentTile;
+    }
+
+    public virtual void Move(Vector2 direction)
+    {
+        if (tileToMoveTo == null)
+            return;
+
+        transform.position = Vector3.Lerp(transform.position, direction, 1f);
+        currentTile = tileToMoveTo;
+        transform.parent = tileToMoveTo.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        moveStep -= Time.fixedDeltaTime;
+        if (moveStep > 0f)
+            return;
+
+        moveStep = 1f;
+        Move(FindPosToMoveToUsingCurrentTile());
+    }
+
+    private Vector2 FindPosToMoveToUsingCurrentTile()
+    {
+        if (currentTile == null)
+            return transform.position;
+
+        List<Tile> neighbours = currentTile.Neighbours;
+
+        Vector2 lowestDistanceScore = new Vector2(999, 999);
+        GameObject player = GameObject.Find("Player");
+       // foreach(Tile t in neighbours)
+       // {
+        //    if(Vector2.Distance(t.transform.position, player.transform.position) < Vector2.Distance(lowestDistanceScore, player.transform.position))
+        //        lowestDistanceScore = t.transform.position;
+        //}
+
+        //return lowestDistanceScore;
+
+        Tile chosenOne = neighbours[new System.Random().Next(0, neighbours.Count)];
+        if (chosenOne.transform.childCount > 0)
+            return transform.position;
+
+        lastTileWasOn = currentTile;
+        tileToMoveTo = chosenOne;
+        return tileToMoveTo.transform.position;
+    }
+}

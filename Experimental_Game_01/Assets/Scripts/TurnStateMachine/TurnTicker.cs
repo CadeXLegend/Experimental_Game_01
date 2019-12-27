2427f2822ca3776b93deedf2a8324fd0d7f94545
CadeXDev <37698676+CadeXLegend@users.noreply.github.com>
2019-12-27 16:51:08 +1000

    Added: - Turn Ticker which just manages how many Ticks have passed in the game - Future Plans.txt contains my plans for future implementations, who woulda thought :o

---
+++ Experimental_Game_01/Assets/Scripts/TurnStateMachine/TurnTicker.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnTicker : MonoBehaviour
+{
+    private static int ticks;
+    public static int Ticks { get => ticks; }
+
+    public delegate void TurnTickEvents();
+    public static event TurnTickEvents OnTick;
+
+    public static void Tick()
+    {
+        ticks++;
+        OnTick?.Invoke();
+    }
+}

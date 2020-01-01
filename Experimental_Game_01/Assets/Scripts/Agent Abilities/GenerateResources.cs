82e4cddea611c1fb81357fdb0ef97379ea610b83
CadeXDev <37698676+CadeXLegend@users.noreply.github.com>
2020-01-02 08:18:53 +1000

    Added: - Sooo, this current version of this script finds a suitable Tile to place a Decoration which is a Resource  (used for the Druid "class")
    Comments:
    - Probs gonna add some more complexity to this algorithm, like a pathfinding check with A* or something to see whether where the Resource will be placed will block any Agents from getting from A-B (or from where they are to any other Agent)
    - I SHOULD PROBABLY ADD COMMENTS TO THIS SCRIPT IN THE FUTURE, PLEASE REMEMBER TO

---
+++ Experimental_Game_01/Assets/Scripts/Agent Abilities/GenerateResources.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Generation;
+using UnityEngine.UI;
+
+public class GenerateResources : MonoBehaviour
+{
+    [SerializeField]
+    private MapGenerator mapGenerator;
+    [SerializeField]
+    private GridAssets assets;
+    [SerializeField]
+    private GridAssetSpawner spawner;
+    [SerializeField]
+    private int generationRadius, localResourceTolerance;
+    [SerializeField]
+    private Slider radiusSlider, toleranceSlider;
+
+    private void Update()
+    {
+        generationRadius = (int)radiusSlider.value;
+        localResourceTolerance = (int)toleranceSlider.value;
+    }
+
+    public virtual void Generate()
+    {
+        bool succeeded = TryGenerate();
+
+        if (!succeeded)
+            TryGenerate();
+        else
+            tilesAttempted.Clear();
+
+        radiusCovered = 0;
+        resourceCountWithinRadiusCovered = 0;
+    }
+
+    private List<Tile> tilesAttempted = new List<Tile>();
+    private bool TryGenerate()
+    {
+        List<Tile> unoccupiedTiles = mapGenerator.Grid.GetAllUnoccupiedTiles();
+        if (tilesAttempted.Count == unoccupiedTiles.Count)
+            return true;
+
+        Tile chosenTile = unoccupiedTiles[Random.Range(0, unoccupiedTiles.Count)];
+        Tile tileToGenerateOn = FindUnoccupiedTileToGenerateOn(chosenTile.Neighbours);
+
+        if (tileToGenerateOn != null)
+        {
+            spawner.SpawnSingleTileDecoration(mapGenerator.MapTheme, "Druid Resource", chosenTile);
+            Debug.Log($"<color=green><b>Druid Spawned Resource on: {chosenTile.name}</b></color>");
+            return true;
+        }
+
+        tilesAttempted.Add(chosenTile);
+        return false;
+    }
+
+    private List<Tile> potentialTilesToSpawnOnto = new List<Tile>();
+    private int radiusCovered = 0;
+    private int resourceCountWithinRadiusCovered = 0;
+    private Tile FindUnoccupiedTileToGenerateOn(List<TileNeighbour> currentNeighbourDepth)
+    {
+        if (radiusCovered >= generationRadius)
+        {
+            if (resourceCountWithinRadiusCovered > localResourceTolerance)
+                return null;
+
+            return potentialTilesToSpawnOnto[Random.Range(0, potentialTilesToSpawnOnto.Count)];
+        }
+
+        radiusCovered++;
+        foreach (TileNeighbour n in currentNeighbourDepth)
+        {
+            if (n.NeighbourTile.IsOccupied)
+            {
+                if (n.NeighbourTile.transform.GetChild(0).tag == "Resource")
+                    resourceCountWithinRadiusCovered++;
+            }
+            else
+            {
+                if (!potentialTilesToSpawnOnto.Contains(n.NeighbourTile))
+                    potentialTilesToSpawnOnto.Add(n.NeighbourTile);
+            }
+
+            if (resourceCountWithinRadiusCovered > localResourceTolerance)
+                return null;
+
+            FindUnoccupiedTileToGenerateOn(n.NeighbourTile.Neighbours);
+        }
+
+        return potentialTilesToSpawnOnto[Random.Range(0, potentialTilesToSpawnOnto.Count)];
+    }
+}

300a1c2b1a5066133411ff46d5ad4e926f8ce798
CadeXDev <37698676+CadeXLegend@users.noreply.github.com>
2020-01-02 08:16:37 +1000

    Changes: - Uhh, System.Linq is here now, okay... might wanna remove this lol

--- Experimental_Game_01/Assets/Scripts/TurnStateMachine/AgentTurnSetter.cs
+++ Experimental_Game_01/Assets/Scripts/TurnStateMachine/AgentTurnSetter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace TurnManagement
@@ -53,6 +54,7 @@ namespace TurnManagement
                 currentAgent++;
                 TurnTicker.Tick();
             }
+
         }
     }
 }

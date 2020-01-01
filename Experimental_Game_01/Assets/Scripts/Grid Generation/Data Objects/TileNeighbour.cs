6b1794e7a2bb38411638863f2c188049f6ebe039
CadeXDev <37698676+CadeXLegend@users.noreply.github.com>
2020-01-02 08:20:42 +1000

    Added: - Nice little class for TileNeighbours - The enum is great for W, A, S, D and directional knowledge for the Graph/Nodes - NeighbourTile is literally the Tile that the Neighbour is

---
+++ Experimental_Game_01/Assets/Scripts/Grid Generation/Data Objects/TileNeighbour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Generation
+{
+    /// <summary>
+    /// Data Object containing Tile neighbour information for Tile related functionality.
+    /// </summary>
+    public class TileNeighbour : MonoBehaviour
+    {
+        public enum NeighbourOrientation
+        {
+            Left,
+            Right,
+            Up,
+            Down,
+        }
+
+        public NeighbourOrientation neighbourOrientation;
+
+        public Tile NeighbourTile { get; set; }
+    }
+}
